42417727341a08ba279e11c83d16cc45bdec9bc3
Samppa50 <silvansami50@gmail.com>
2024-07-08 19:14:39 +0300

    start screen!

--- scenes/startscreen/StartScreen.gd
+++ scenes/startscreen/StartScreen.gd
@@ -1,4 +1,4 @@
-class_name StartScreen extends Control
+class_name StartScreen extends Node
 
 func _on_button_button_up() -> void:
 	SceneManager.load_new_scene("res://scenes/openworld/level1.tscn","fade_to_black")
