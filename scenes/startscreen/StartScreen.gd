8cc5a0d62349165d7071ddd44801cd51e1dc023a
Samppa50 <silvansami50@gmail.com>
2024-07-09 17:05:02 +0300

    kamera muutoksia ja startscreen

--- scenes/startscreen/StartScreen.gd
+++ scenes/startscreen/StartScreen.gd
@@ -1,7 +1,7 @@
 class_name StartScreen extends Node
 
 func _on_button_button_up() -> void:
-	SceneManager.load_new_scene("res://scenes/openworld/level1.tscn","fade_to_black")
+	SceneManager.load_new_scene("res://scenes/openworld/alotushuonetesti_1.tscn","fade_to_black")
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
